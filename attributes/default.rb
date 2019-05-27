619137a35561cc9563afbb2a0939260fadb338e1
Fabio Buso <buso.fabio@gmail.com>
2019-05-27 15:51:34 +0200

    bump versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ include_attribute "kzookeeper"
 
 default['hive2']['user']                    = node['install']['user'].empty? ? "hive" : node['install']['user']
 default['hive2']['group']                   = node['install']['user'].empty? ? node['hops']['group'] : node['install']['user']
-default['hive2']['version']                 = "2.3.0.2-SNAPSHOT"
+default['hive2']['version']                 = "3.0.0.1"
 default['hive2']['url']                     = "#{node['download_url']}/apache-hive-#{node['hive2']['version']}-bin.tar.gz"
 default['hive2']['port']                    = "9084"
 default['hive2']['portssl']                 = "9085"
@@ -33,7 +33,7 @@ default['hive2']['hive_cleaner']['pid_file']  = "/tmp/hc.pid"
 
 default['tez']['user']                    =  node['install']['user'].empty? ? "tez" : node['install']['user']
 default['tez']['group']                   =  node['hops']['group']
-default['tez']['version']                 = "0.9.1.1-SNAPSHOT"
+default['tez']['version']                 = "0.9.1.1"
 default['tez']['url']                     = "#{node['download_url']}/apache-tez-#{node['tez']['version']}.tar.gz"
 default['tez']['dir']                     =  node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 default['tez']['home']                    =  node['tez']['dir'] + "/apache-tez-" + node['tez']['version']
@@ -43,7 +43,7 @@ default['tez']['conf_dir']                =  node['tez']['base_dir'] + "/conf"
 
 default['slider']['user']                    =  node['install']['user'].empty? ? "slider" : node['install']['user']
 default['slider']['group']                   =  node['hops']['group']
-default['slider']['version']                 = "0.93.0-incubating-SNAPSHOT"
+default['slider']['version']                 = "0.93.1-incubating-SNAPSHOT"
 default['slider']['url']                     = "#{node['download_url']}/slider-#{node['slider']['version']}-all.tar.gz"
 default['slider']['dir']                     =  node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 default['slider']['home']                    =  node['slider']['dir'] + "/apache-slider-" + node['slider']['version']
@@ -51,7 +51,7 @@ default['slider']['base_dir']                =  node['slider']['dir'] + "/apache
 
 default['hive2']['execution_mode']         = "llap"
 default['llap']['cluster_name']            = "hops-llap"
-default['llap']['execution_mode']          = "auto"
+default['llap']['execution_mode']          = "none"
 
 default['tez']['session_per_queue']     = 100
 
